c0e123b72e4f6e6eacc2e0da04718c9016ccd2ab
Muhammed Fidan <Muhammed Fidan@LAPTOP-VIMI1TDU>
2021-01-17 22:31:35 +0400

    Add project files.

---
+++ OutKeyword/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace OutKeyword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            Console.WriteLine("i'nin önceki değeri:" + i);
+            SonrakiAdiAl(i);
+            Console.WriteLine("i'nin mevcut değeri:" + i);
+            Console.WriteLine("------------OUT KEYWORD-----------");
+            Console.WriteLine("i'nin önceki değeri::" + i);
+            SonrakiAdiAl2(out i);
+            Console.WriteLine("i'nin mevcut değeri:" + i);
+
+
+        }
+        public static string SonrakiAdiAl(int sayi)
+        {
+            sayi = 1;
+            string returnText = "Next-" + sayi.ToString();
+            return returnText;
+        }
+
+        //Şimdi out keywordu ile görelim birde..
+        public static string SonrakiAdiAl2(out int sayi)
+        {
+            sayi = 1;
+            string returnText = "Next-" + sayi.ToString();
+            return returnText;
+        }
+        //out ve ref keyword birbirine çok benzer. Out keywordu kullanabilmek için kesinlikle metot içinde değişkene bir değer ataması yapılmak zorundadır. 
+    }
+
+   
+}
---
+++ RefAndOutKeywords/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace RefAndOutKeywords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            Console.WriteLine("Metot çağırılmadan önce a'nın değeri: " + a);
+            VeriyiAl(a);
+            Console.WriteLine("Metot çağırıldıktan sonra a'nın değeri: " + a);
+            Console.WriteLine("-------REF KEYWORD----------");
+            Console.WriteLine("Metot çağırılmadan önce a'nın değeri: " + a);
+            VeriyiAl2(ref a);
+            Console.WriteLine("Metot çağırıldıktan sonra a'nın değeri: " + a);
+            Console.ReadKey();
+        }
+
+        public static void VeriyiAl(int a)
+        {
+            a = a + 20;
+            Console.WriteLine("a'nın metot içindeki değeri: " + a);
+        }
+
+        //Şimdi a'değerini ref keywordu ile çağırıp farkı göreceğiz.
+        public static void VeriyiAl2(ref int a)
+        {
+            
+            a = a + 20;
+            Console.WriteLine("a'nın metot içindeki değeri: " + a);
+        }
+        //Gördüğünüz gibi a'nın değeri bu sefer 30 oldu. Aslında biz a'nın değerini değiştirmedik, metot ile paylaşılan a'nın orjinal değerini güncellemiş olduk. 
+        //Ref keywordu kullanımı; parametrenin gerçek değerle değil, referansla geçmesine neden olmasıdır.
+    }
+}
+
